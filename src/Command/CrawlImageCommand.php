ba3a410aa83c6be8cfa5f457df3b8a7de3161ce2
kirya <cloudkserg11@gmail.com>
2017-07-02 23:22:32 +0300

    fixed #abba

--- src/Command/CrawlImageCommand.php
+++ src/Command/CrawlImageCommand.php
@@ -10,20 +10,28 @@ namespace src\Command;
 
 
 use Carbon\Carbon;
+use Dotenv\Dotenv;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\TransferStats;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DomCrawler\Crawler;
+class_alias('\RedBeanPHP\R','\R');
+use R;
+
 
 class CrawlImageCommand extends Command
 {
 
-    const URL = 'https://arxip.com/project/42286/architecture-individualnyiy-jiloy-dom-v-pnovodarino-jiloy-dom-jile-kottedj-osobnyak-modernizm-modernizm-minimalizm-minimalizm-2-etaja-6m-500-1000-m2-42286/';
+    const LIMIT_TIME = 5;
+
+    const DSN = 'mysql:host=localhost;dbname=%s';
+    //const URL = 'https://arxip.com/project/';
     //const URL = 'https://cdn.arxip.com/resize/object_detail/arxip/239/2399838e408f33c5ae7282be51f2fce1.jpg';
-    //const URL = 'https://beta2.arxip.com/project/42286/architecture-individualnyiy-jiloy-dom-v-pnovodarino-jiloy-dom-jile-kottedj-osobnyak-modernizm-modernizm-minimalizm-minimalizm-2-etaja-6m-500-1000-m2-42286/?super=1';
+    const URL = 'https://arxip.com/project/%s';
     //const URL = 'https://beta2.arxip.com/index.html';
     //const URL = 'http://beta2.arxip.com:3000/';
 
@@ -33,30 +41,82 @@ class CrawlImageCommand extends Command
     }
 
 
+    private function loadConfigDb()
+    {
+        $dotenv = new Dotenv(dirname(dirname(__DIR__)));
+        $dotenv->load();
+    }
+
+    private function connectDb()
+    {
+        $this->loadConfigDb();
+        R::setup(sprintf(self::DSN, $_ENV['DATABASE_NAME']), $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASS']);
+    }
+
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
 
+
+        $this->connectDb();
+        $collection = R::findCollection('arxip_objects', "ACTIVE='Y' ORDER BY id DESC");
+        while ($itemCol = $collection->next()) {
+            $item = $itemCol->getProperties()['id'];
+            $this->parseUrl($item, $io);
+        }
+    }
+
+    private function parseUrl($id, SymfonyStyle $io)
+    {
+        $url = sprintf(self::URL, $id);
         $io->section(
-            sprintf('[url:%s] Begin image crawler [time:%s]', self::URL, Carbon::create()->toDateTimeString())
+            sprintf('[url:%s] Begin image crawler [time:%s]', $url, Carbon::create()->toDateTimeString())
         );
 
-        $client = new Client();
-        $response = $client->request('GET', self::URL, [
-            'on_stats' => function (TransferStats $stats) use ($io) {
-                $handlerStats = $stats->getHandlerStats();
-                //var_dump($handlerStats);
-                $io->writeln($handlerStats['total_time']);
-            }
-        ]);
+        $response = $this->request($url, $io);
+        if (empty($response)) {
+            $io->error(sprintf('[url:%s] error not load', $url));
+            throw new \ErrorException('error');
+        }
 
-//        $crawler = new Crawler((string)$response->getBody());
-//        $images = $crawler->selectImage('img');
-//        var_dump($images);
+        $crawler = new Crawler((string)$response->getBody());
+        collect($crawler->filter('img'))->map(function (\DOMElement $img) use ($io) {
+            $url = $img->getAttribute('src');
+            if (!empty($url)) {
+                $this->request($url, $io);
+            }
+        });
 
         $io->section(
             sprintf('[url:%s] End image crawler [time:%s]', self::URL, Carbon::create()->toDateTimeString())
         );
+    }
+    private function request($url, SymfonyStyle $io)
+    {
+        if (substr($url, 0, 4) !== 'http') {
+            $url = 'https://arxip.com' . $url;
+        }
+
+
+
+        $client = new Client();
+        try {
+            $response = $client->request('GET', $url, [
+                'on_stats' => function (TransferStats $stats) use ($io, $url) {
+                    $handlerStats = $stats->getHandlerStats();
+                    $totalTime = $handlerStats['total_time'];
+                    if ($totalTime > self::LIMIT_TIME) {
+                        $io->error(sprintf('ERROR[%s] %s time:%s', Carbon::create()->toDateTimeString(), $url, $totalTime));
+                        throw new \ErrorException('error');
+                    }
+                    $io->writeln(sprintf("\t url=%s [%s]", $url, $totalTime));
+                }
+            ]);
+            return $response;
+        } catch (ClientException $e) {
+            return null;
+        }
     }
 
 
